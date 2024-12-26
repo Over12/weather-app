23f476be589959ea1aefe60409b62344656a3511
OverCode <avalosfigueroapedro@gmail.com>
2024-12-25 20:50:28 -0500

    Agregar constantes de condiciones climáticas con colores asociados

---
+++ src/constants/CONDITIONS.js
@@ -0,0 +1,10 @@
+export const CONDITIONS = {
+  COLORS: {
+    Sunny: "from-yellow-400 to-yellow-600",
+    Clear: "from-blue-400 to-blue-600",
+    Overcast: "from-gray-400 to-gray-600",
+    Cloudy: "from-gray-500 to-gray-700",
+    PartlyCloudy: "from-blue-400 to-gray-700",
+    Freezingfog: "from-cyan-400 to-blue-700",
+  }
+}
