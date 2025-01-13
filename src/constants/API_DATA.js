79a25451b907bf9d0407dea756796125b83ca8e6
OverCode <avalosfigueroapedro@gmail.com>
2025-01-12 19:18:22 -0500

    Actualizar la URL de la API en API_DATA para usar HTTPS

--- src/constants/API_DATA.js
+++ src/constants/API_DATA.js
@@ -1,4 +1,4 @@
 export const API_DATA = {
-  url: "http://api.weatherapi.com/v1/current.json",
-  key: import.meta.env.VITE_API_KEY
+  url: "https://api.weatherapi.com/v1/current.json",
+  key: import.meta.env.VITE_API_KEY,
 }
