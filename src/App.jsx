a9a9d0969df4914cffae250ab3ca9c82407bcab4
OverCode <avalosfigueroapedro@gmail.com>
2025-10-02 17:39:15 -0500

    Agregar componente Footer y actualizar App para incluirlo en la vista

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import ThemeButton from './components/common/ThemeButton'
 import Home from './components/ui/Home'
+import Footer from './components/ui/Footer'
 
 function App() {
   const [theme, setTheme] = useState(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
@@ -12,6 +13,7 @@ function App() {
   return (
     <main className={`relative bg-background text-text ${theme} transition-colors`}>
       <Home />
+      <Footer />
       <ThemeButton toggleTheme={toggleTheme} theme={theme} />
     </main>
   )
---
+++ src/components/ui/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className='absolute bottom-0 sm:text-center sm:w-full p-5'>
+      <span className='text-sm sm:text-base'>© 2025 hecho con ❤️ por <a href='https://overcode.netlify.app/' target='_blank' rel='noopener noreferrer' className='bg-clip-text bg-gradient-to-r from-[#748ca8] to-[#6c58c0] text-transparent font-bold'>Overcode</a></span>
+    </footer>
+  )
+}
